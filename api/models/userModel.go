c59c2218176c712e81485c24f047ab535512133e
CoderBlaine <blaine@worldweb.com>
2019-12-04 16:34:39 -0700

    Cleaed up files and added documentation

--- api/models/userModel.go
+++ api/models/userModel.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"log"
 	"setuphelper/api/utilities"
 	"strings"
@@ -12,6 +13,7 @@ import (
 )
 
 type (
+	//UserModel - User Values
 	UserModel struct {
 		//ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 		ID string `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -26,29 +28,17 @@ type (
 		CreationTime
 	}
 
-	UserModelTest struct {
-		ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-		UserID       string             `json:"userID"`
-		UserName     string             `json:"userName"`
-		Password     string             `json:"password"`
-		SecurityList []UserSecurity     `json:"securityList"`
-		ContactID    string             `json:"contactID"`
-	}
-
+	//UserSecurty - UserSecurity Levels
 	UserSecurity struct {
 		Level string `json:"level"`
 	}
 )
 
 var (
-	users           = map[string]*UserModel{}
-	userseq         = 1
+	//local variable use to set the collection name for
+	//queries against the user model
 	userTableConfig = &TableConfig{"users"}
 )
-
-func DeleteUser(id string) {
-	delete(users, id)
-}
 
 // GetUser - Return the userModel of the Object ID
 func GetUser(id primitive.ObjectID) (UserModel, error) {
@@ -66,19 +56,59 @@ func GetUser(id primitive.ObjectID) (UserModel, error) {
 	return userModel, err
 }
 
-func GetUserList() map[string]*UserModel {
-	return users
+// GetUserList - Return the userModel of the Object ID
+func GetUserList() []UserModel {
+	filter := bson.M{}
+	cursor, err := utilities.DatabaseObj.Find(userTableConfig.Name, filter)
+	defer cursor.Close(context.Background())
+
+	if err != nil {
+		utilities.PrintDebug("Get User List Error", err)
+		//return []&UserModel{}
+	}
+
+	var userList []UserModel
+	for cursor.Next(context.Background()) {
+		// To decode into a struct, use cursor.Decode()
+		userModel := UserModel{}
+
+		err := cursor.Decode(&userModel)
+		if err != nil {
+			log.Fatal("GetUserList", err)
+		}
+
+		userList = append(userList, userModel)
+	}
+
+	return userList
 }
 
+// Delete - Save the User
+func (m *UserModel) Delete() error {
+	log.Print("User Model Delete")
+
+	filter := bson.M{"_id": m.GetID()}
+	result := utilities.DatabaseObj.FindOneAndDelete(userTableConfig.Name, filter)
+
+	if result.Err() != nil {
+		utilities.PrintDebug("Delete Error", result.Err)
+	}
+
+	return result.Err()
+}
+
+//GetID - Return the ID string
 func (m *UserModel) GetID() string {
 	//id, _ := primitive.ObjectIDFromHex(m.ID)
 	return m.ID
 }
 
+//GetFullName - Return the full name of the user
 func (m *UserModel) GetFullName() string {
 	return strings.Join([]string{m.FirstName, m.LastName}, " ")
 }
 
+//IsAllowedToLogin - Determine if the user is allowed to login
 func (m *UserModel) IsAllowedToLogin() bool {
 	//id, _ := primitive.ObjectIDFromHex(m.ID)
 	filter := bson.M{
@@ -96,7 +126,7 @@ func (m *UserModel) IsAllowedToLogin() bool {
 	return true
 }
 
-//func (m *UserModel) SetID(id string) {
+//SetID - Set the ID
 func (m *UserModel) SetID(id primitive.ObjectID) {
 	//m.ID = id.Hex()
 	m.ID = id.String()
@@ -121,7 +151,7 @@ func (m *UserModel) Save() error {
 	return nil
 }
 
-//func (m *UserModel) Update(update bson.D) error {
+//Update - Save the current model
 func (m *UserModel) Update() error {
 	//id, _ := primitive.ObjectIDFromHex(m.GetID())
 
